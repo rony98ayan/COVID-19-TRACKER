fc0431e736fe784702837c683ffba97dda7324a8
rony98ayan <64652650+rony98ayan@users.noreply.github.com>
2023-06-16 17:03:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Covid from './Component/Covid';
+
+function App() {
+  return (
+    <div className="App">
+      <Covid></Covid>
+    </div>
+  );
+}
+
+export default App;
